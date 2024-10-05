9766e3dde3573b4ee73c35a214a85d3dfd2a5fe9
N3nT <bar55v71@interia.pl>
2024-10-05 17:52:38 +0200

    Start work with nav

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php include'nav.html'; ?>
+</body>
+<script src="https://kit.fontawesome.com/82d15cf176.js" crossorigin="anonymous"></script>
+</html>
